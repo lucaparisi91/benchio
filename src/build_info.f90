5d435c232705c5c8b29ba7c8c86452b6bd3ac60c
lucaparisi91 <luca.parisi91@gamil.com>
2023-06-08 17:21:31 +0100

    increased max path length

---
+++ src/build_info.f90
@@ -0,0 +1,5 @@
+
+module build_info
+    integer, parameter :: max_path_length = 128
+    character(max_path_length) :: benchio_install_dir = "/work/z19/z19/lparisi/spack/benchio_install"
+end module
